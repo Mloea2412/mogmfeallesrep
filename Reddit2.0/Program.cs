bf863f676b91f9740c47d38653ec82af05321712
Mloea2412 <103989630+Mloea2412@users.noreply.github.com>
2023-10-09 11:45:41 +0200

    program.cs, dataservice udvidet, seeddata tilføjet

--- Reddit2.0/Program.cs
+++ Reddit2.0/Program.cs
@@ -1,6 +1,108 @@
+using Microsoft.EntityFrameworkCore;
+using Reddit2._0.Model;
+using Reddit2._0.Service;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Sætter CORS så API'en kan bruges fra andre domæner
+var AllowSomeStuff = "_AllowSomeStuff";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowSomeStuff, builder => {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
+// Tilføj DbContext factory som service.
+builder.Services.AddDbContext<RedditContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("ContextSQLite")));
+
+// Viser flotte fejlbeskeder i browseren hvis der kommer fejl fra databasen
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// Tilføj DataService så den kan bruges i endpoints
+builder.Services.AddScoped<DataService>();
+
+// Dette kode kan bruges til at fjerne "cykler" i JSON objekterne.
+/*
+builder.Services.Configure<JsonOptions>(options =>
+{
+    // Her kan man fjerne fejl der opstår, når man returnerer JSON med objekter,
+    // der refererer til hinanden i en cykel.
+    // (altså dobbelrettede associeringer)
+    options.SerializerOptions.ReferenceHandler = 
+        System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+});
+*/
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
+// Seed data hvis nødvendigt.
+using (var scope = app.Services.CreateScope())
+{
+    var dataService = scope.ServiceProvider.GetRequiredService<DataService>();
+    dataService.SeedData(); // Fylder data på, hvis databasen er tom. Ellers ikke.
+}
+
+app.UseHttpsRedirection();
+app.UseCors(AllowSomeStuff);
+
+// Middlware der kører før hver request. Sætter ContentType for alle responses til "JSON".
+app.Use(async (context, next) =>
+{
+    context.Response.ContentType = "application/json; charset=utf-8";
+    await next(context);
+});
+
+// DataService fåes via "Dependency Injection" (DI)
+app.MapGet("/", (DataService service) =>
+{
+    return new { message = "Hello World!" };
+
+});
+
+// POSTS
+app.MapGet("/api/posts", (DataService service) =>
+{
+    return service.GetPosts();
+});
+
+// POST PÅ ID
+app.MapGet("/api/post/{id}", (DataService service, int postid) =>
+{
+    return service.GetPost(postid);
+});
+
+// COMMENTS
+
+app.MapGet("/api/comments", (DataService service) =>
+{
+    return service.GetComments();
+});
+
+app.MapGet("/api/commment/{id}", (DataService service, int commentid) =>
+{
+    return service.GetComment(commentid);
+});
+
+app.MapGet("/api/users", (DataService service) =>
+{
+    return service.GetUsers();
+});
+
+app.MapGet("/api/user/{id}", (DataService service, int userid) =>
+{
+    return service.GetUsers(userid);
+});
+
+/*
+app.MapPost("/api/....", (DataService service, ......) =>
+{
+
+})
+*/
 app.Run();
--- Reddit2.0/Service/DataService.cs
+++ Reddit2.0/Service/DataService.cs
@@ -13,40 +13,78 @@ namespace Reddit2._0.Service
             this.db = db; // Konstruktøren modtager en RedditContext-injektion og tildeler den til det private felt.
         }
 
+
+        // SEED DATA HER - ET BUD PÅ SEEDDATA
+
+        public void SeedData()
+        {
+            Post post = db.Posts.FirstOrDefault()!;
+            if (post == null)
+            {
+                User user1 = new User("M");
+                post = new Post { PostId = 1, Title = "Post om alverden", User = user1, Text = "En vigtig post om alverden", Downvote = 2, Upvote = 12, NumberOfVotes = 14 };
+                db.Add(post);
+                db.SaveChanges();
+            }
+
+            Comment comment = db.Comments.FirstOrDefault()!;
+            if (comment == null)
+            {
+                comment = new Comment { CommentId = 1, Text = "Det lyder bare spændende", Downvote = 3, Upvote = 5, NumberOfVotes = 8 };
+                db.Add(comment);
+                db.SaveChanges();
+
+            }
+
+            User user = db.Users.FirstOrDefault();
+            if (user == null)
+            {
+                User user1 = new User("Testuser");
+                User user2 = new User("Magnus");
+                User user3 = new User("M-L");
+                User user4 = new User("TestUser4");
+                db.Add(user1);
+                db.Add(user2);
+                db.Add(user3);
+                db.Add(user4);
+                db.SaveChanges();
+
+            }
+        }
+
         // Henter alle poster som en liste
-        public List<Post> GetAllPosts()
+        public List<Post> GetPosts()
         {
             return db.Posts.ToList(); // Returnerer en liste af alle poster fra databasen ved at konvertere dem til en liste.
         }
 
-        /* FEJL SKAL UDBEDRES, DERFOR ER DEN UDKOMMENTERET
         // Henter en specifik post baseret på dens id
-        public Post GetPostById()
+        public Post GetPost()
         {
-            return db.Posts.Where(p => p.PostId == postid).FirstOrDefault()!;
+           return db.Posts.Where(p => p.PostId == postid).FirstOrDefault()!;
         }
-        */
+        
 
         // Henter alle kommentarer som en liste
-        public List<Comment> GetAllComments()
+        public List<Comment> GetComments()
         {
             return db.Comments.ToList(); // Returnerer en liste af alle kommentarer fra databasen ved at konvertere dem til en liste.
         }
 
         // Henter en specifik kommentar baseret på dens id
-        public Comment GetCommentById(int commentid)
+        public Comment GetComment(int commentid)
         {
             return db.Comments.Where(c => c.CommentId == commentid).FirstOrDefault()!; // Henter en kommentar fra databasen baseret på dens id og returnerer den.
         }
 
         // Henter alle brugere som en liste
-        public List<User> GetAllUsers()
+        public List<User> GetUsers()
         {
             return db.Users.ToList(); // Returnerer en liste af alle brugere fra databasen ved at konvertere dem til en liste.
         }
 
         // Henter en specifik bruger baseret på dens id
-        public User GetUserById(int userid)
+        public User GetUserId(int userid)
         {
             return db.Users.Where(u => u.UserId == userid).FirstOrDefault()!; // Henter en bruger fra databasen baseret på dens id og returnerer den.
         }
